5dadfd45cd7f291c0fe012446066bc7c69b9de03
joaopedev <joaopedev@outlook.com>
2023-12-10 22:42:23 -0300

    videoCards fixin

--- src/components/videoCards.tsx
+++ src/components/videoCards.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Grid, GridItem, VStack } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import VimeoPlayer from "./vimeoVideos";
 
 const VideoCards: React.FC = () => {
